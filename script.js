09c550f673ddd8d159ab345ed429266d8d15e101
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-15 15:34:18 -0400

    Add functions to create grid, color squares and reset grid.

---
+++ script.js
@@ -0,0 +1,40 @@
+// get dom nodes
+const gridDimension = document.querySelector('#grid-dimension-slider');
+const gridContainer = document.querySelector('#grid-container');
+const gridColor = document.querySelector('#grid-color');
+
+function createGrid (dimension) {
+    let numberOfSquares = gridDimension ** 2;
+    let gridSize = gridContainer.style.width;
+    let squareWidth = gridSize / dimension;
+    for (let i = 0; i < numberOfSquares; i++){
+        let square = document.createElement('div');
+        square.style.width = squareWidth;
+        square.style.height = squareWidth;
+        gridContainer.appendChild(square);
+
+        // add event listener for each square
+        square.addEventListener('mouseover', colorSquare);
+    }
+}
+
+function colorSquare (event, color = gridColor.value) {
+    let currentColor = color;
+    event.target.style.backgroundColor = currentColor;
+}
+
+gridDimension.addEventListener('change', resetGrid);
+
+function resetGrid () {
+    // clear grid first
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+
+    // recreate the grid with current dimensions
+    let dimension = gridDimension.value;
+    createGrid(dimension);
+}
+
+// initialize grid
+createGrid(gridDimension.value);
