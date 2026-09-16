598bffdb28b4d0cc2740e102df8e5cbef0da86d3
sen-oy <313918650+sen-oy@users.noreply.github.com>
2026-09-16 11:18:54 -0400

    Edit grid squares class to use border box box sizing.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const gridColor = document.querySelector('#grid-color');
 
 // helper functions
 function getContainerWidth (container) {
-    return container.offsetWidth; // we're using border box sizing
+    return parseInt(container.clientWidth); // we're using border box sizing
 }
 
 function getGridColorValue (colorPicker) {
@@ -13,46 +13,52 @@ function getGridColorValue (colorPicker) {
 }
 
 function getGridDimensions (dimensionInput) {
-    return dimensionInput.value;
+    return parseInt(dimensionInput.value);
 }
 
-function createGrid (dimension) {
-    let numberOfSquares = parseInt(dimension) ** 2;
-    let gridSize = parseInt(gridContainer.clientWidth);
-    let squareWidth = gridSize / dimension;
+function createGridSquare (squareWidth) {
+    let square = document.createElement('div');
+    square.classList.add('grid-square');
+    square.style.width = `${squareWidth}px`;
+    square.style.height = `${squareWidth}px`;
+
+    return square
+}
+
+function createGrid (dimensionInput, container) {
+    let numberOfSquares = parseInt(dimensionInput) ** 2;
+    let gridSize = getContainerWidth(container);
+    let squareWidth = gridSize / dimensionInput;
+    
     for (let i = 0; i < numberOfSquares; i++){
-        let square = document.createElement('div');
-        square.classList.add('grid-square');
-        square.style.width = squareWidth;
-        square.style.height = squareWidth;
-        gridContainer.appendChild(square);
+        let square = createGridSquare(squareWidth);
+        container.appendChild(square);
 
         // add event listener for each square
         square.addEventListener('mouseover', colorSquare);
     }
 }
 
-function colorSquare (event, color = gridColor.value) {
-    let currentColor = color;
+function colorSquare (event, currentColor = getGridColorValue(gridColor)) {
     event.target.style.backgroundColor = currentColor;
 }
 
-gridDimension.addEventListener('change', resetGrid);
+gridDimension.addEventListener('change', () => {
+    resetGrid(gridContainer, gridDimension);
+});
 
-function resetGrid () {
+function resetGrid (container, dimensionInput) {
     // clear grid first
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.lastChild);
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
 
     // recreate the grid with current dimensions
-    let dimension = gridDimension.value;
-    createGrid(dimension);
+    let dimension = getGridDimensions(dimensionInput);
+    createGrid(dimension, container);
 }
 
 // initialize grid
 window.addEventListener('load', () => {
-    createGrid(gridDimension.value);
+    resetGrid(gridContainer, gridDimension);
 })
-
-// add helper functions to decouple variables and functions
